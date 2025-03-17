0045ad31d6ffd7552a9d1b4f235e1b196e4a61ac
michael <dodan.kwon@gmail.com>
2025-03-17 23:47:57 +0900

    feat: database설정정

--- java/src/main/java/img/camera/tools/domain/controller/MainController.java
+++ java/src/main/java/img/camera/tools/domain/controller/MainController.java
@@ -1,7 +1,6 @@
 package img.camera.tools.domain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
